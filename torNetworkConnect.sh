e237bc3665dd94c2559474f700fd72a2d267ce19
Eike Mücke <eimimue@googlemail.com>
2017-02-13 16:30:32 +0100

    added sources

--- torNetworkConnect.sh
+++ torNetworkConnect.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # This script connects you to the tor network with your provided
-# interface of choice. 
-INTERFACE=Thunderbolt\ Ethernet
+# interface of choice.
+# https://leonid.shevtsov.me/post/an-easy-way-to-use-tor-on-os-x/
+# type "networksetup -listallnetworkservices" in terminal to retrieve a
+# list of available connection names
+
+# CTRL+C to disconnect and restore settings
+
+INTERFACE=Thunderbolt\ Ethernet #Wi-Fi, ...
+
 networksetup -setsocksfirewallproxy "${INTERFACE}" 127.0.0.1 9050 off
 networksetup -setsocksfirewallproxystate "${INTERFACE}" on
 tor
