dcf518a0553b45f3505a64d6d37cc8699516e13a
Eike Mücke <eimimue@googlemail.com>
2017-02-13 16:26:46 +0100

    added anotations

--- torNetworkConnect.sh
+++ torNetworkConnect.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+
+# This script connects you to the tor network with your provided
+# interface of choice. 
 INTERFACE=Thunderbolt\ Ethernet
-#networksetup -setsocksfirewallproxy $INTERFACE 127.0.0.1 9050 off
-#networksetup -setsocksfirewallproxystate $INTERFACE on
-#tor
-#networksetup -setsocksfirewallproxystate $INTERFACE off
 networksetup -setsocksfirewallproxy "${INTERFACE}" 127.0.0.1 9050 off
 networksetup -setsocksfirewallproxystate "${INTERFACE}" on
 tor
